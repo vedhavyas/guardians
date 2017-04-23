8cc927268089055d47c9d510527fb28b269bd5cc
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2017-04-23 12:52:16 +0530

    remove read-only

--- users/admin.py
+++ users/admin.py
@@ -5,9 +5,4 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    readonly_fields = [
-        'id',
-        'username',
-        'password',
-        'last_login',
-    ]
+    pass
