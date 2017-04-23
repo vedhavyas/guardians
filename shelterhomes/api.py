2da3c197206901400da44e133b892740d1714346
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2017-04-23 09:48:41 +0530

    remove the delete mixin

--- shelterhomes/api.py
+++ shelterhomes/api.py
@@ -1,10 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
 from shelterhomes.models import ShelterHome
 from shelterhomes.serializers import ShelterHomeSerializer
 
 
-class ShelterHomeViewSet(ModelViewSet):
+class ShelterHomeViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
     serializer_class = ShelterHomeSerializer
     queryset = ShelterHome.objects.all()
     lookup_field = 'id'
